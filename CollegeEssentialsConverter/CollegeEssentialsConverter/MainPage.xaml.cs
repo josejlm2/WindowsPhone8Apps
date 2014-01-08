b59dd2237dc92a9285f35f702d90a20cc10a343e
josejlm2 <josejlm2@hotmail.com>
2014-01-08 00:28:28 -0600

    basic converter functionality complete

--- CollegeEssentialsConverter/CollegeEssentialsConverter/MainPage.xaml.cs
+++ CollegeEssentialsConverter/CollegeEssentialsConverter/MainPage.xaml.cs
@@ -8,34 +8,149 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using CollegeEssentialsConverter.Resources;
+using WPET;
 
 namespace CollegeEssentialsConverter
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private bool convertValue1;
+        Settings<string> conversionType = new Settings<string>("ConversionType", "temperature");
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void SetupLabels()
+        {
+            if (conversionType.Value == "temperature")
+            {
+                this.PageTitle.Text = "temperature";
+                textBlock1.Text = "Fahrenheit";
+                textBlock2.Text = "Celsius";
+            }
+            else
+            {
+                this.PageTitle.Text = "spoons";
+                textBlock1.Text = "Teaspoons";
+                textBlock2.Text = "Tablespoons";
+            }
+        }
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
 
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            convertValue1 = true;
+            SetupLabels();
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            switch (conversionType.Value)
+            {
+                case "temperature":
+                    {
+                        if (convertValue1)
+                        {
+                            textBox2.Text = "";
+                            try
+                            {
+                                double fahrenheightValue = double.Parse(textBox1.Text);
+                                double celsiusValue = (fahrenheightValue - 32) * (5.0 / 9.0);
+                                textBox2.Text = celsiusValue.ToString("F");
+                            }
+                            catch (Exception)
+                            {
+                                textBlock2.Text = "Error";
+                            }
+                        }
+                        else
+                        {
+                            textBox1.Text = "";
+                            try
+                            {
+                                double celsiusValue = double.Parse(textBox2.Text);
+                                double fahrenheitValue = (celsiusValue * (9.0 / 5.0)) + 32;
+                                textBox1.Text = fahrenheitValue.ToString("F");
+                            }
+                            catch (Exception)
+                            {
+                                textBlock1.Text = "Error";
+                            }
+                        }
+                    }
+                    break;
+
+                case "spoons":
+                    {
+                        if (convertValue1)
+                        {
+                            textBox2.Text = "";
+                            try
+                            {
+                                double teaspoons = double.Parse(textBox1.Text);
+                                double tablespoons = teaspoons / 3;
+                                textBox2.Text = tablespoons.ToString("F");
+                            }
+                            catch (Exception)
+                            {
+                                textBlock2.Text = "Error";
+                            }
+                        }
+                        else
+                        {
+                            textBox1.Text = "";
+                            try
+                            {
+                                double tablespoons = double.Parse(textBox2.Text);
+                                double teaspoons = tablespoons * 3;
+                                textBox1.Text = teaspoons.ToString("F");
+                            }
+                            catch (Exception)
+                            {
+                                textBlock1.Text = "Error";
+                            }
+                        }
+                    }
+                    break;
+            }
+            this.Focus();
+        }
+
+        private void Value1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            convertValue1 = true;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void Value2_GotFocus(object sender, RoutedEventArgs e)
+        {
+            convertValue1 = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void ChooseTemperatureConversion_Click(object sender, EventArgs e)
+        {
+            conversionType.Value = "temperature";
+            SetupLabels();
+        }
+
+        private void ChooseSpoonConversion_Click(object sender, EventArgs e)
+        {
+            conversionType.Value = "spoons";
+            SetupLabels();
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        { 
+            //Navigates to a new page
+            NavigationService.Navigate(new Uri("/about.xaml", UriKind.Relative));
+        }
+
     }
+
 }
---
+++ CollegeEssentialsConverter/CollegeEssentialsConverter/about.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+namespace CollegeEssentialsConverter
+{
+    public partial class about : PhoneApplicationPage
+    {
+        public about()
+        {
+            InitializeComponent();
+            this.textBlock1.Text = "Version 1.0\r\n\r\nBuilt by Dream the Impossible Studios \r\n\r\n Windows Phone Applications.";
+        }
+
+        private void Suggestion_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Phone.Tasks.EmailComposeTask emailTask = new Microsoft.Phone.Tasks.EmailComposeTask();
+            emailTask.Subject = "Suggestion for COLLEGE ESSENTIALS CONVERTER v1.0";
+            emailTask.To = "dreamtheimpossiblestudios@gmail.com";
+            emailTask.Show();
+        }
+
+        private void More_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask task = new WebBrowserTask();
+            task.Uri = new Uri("http://www.windowsphone.com/en-us/store", UriKind.Absolute);
+            task.Show();
+        }
+
+        private void Learn_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask task = new WebBrowserTask();
+            task.Uri = new Uri("http://www.josemanriquez.com", UriKind.Absolute);
+            task.Show();
+        }
+    }
+}
